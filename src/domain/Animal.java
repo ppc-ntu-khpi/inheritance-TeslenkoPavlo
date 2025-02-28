4f002e467a7797e6aa28b57dbc6a8e5a523c506e
PAVLO TESLENKO <149189014+TeslenkoPavlo@users.noreply.github.com>
2025-02-28 17:45:00 +0200

    Add files via upload

---
+++ src/domain/Animal.java
@@ -0,0 +1,32 @@
+package domain;
+
+public class Animal {
+    protected String name;
+    protected int age;
+    
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+    
+    public void sleep() {
+        System.out.println(name + " is sleeping.");
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    @Override
+    public String toString() {
+        return "Animal:\nName: " + name + "\nAge: " + age;
+    }
+}
